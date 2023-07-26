299ea658cebea2b76d09b37b2ce51658f8d78536
Sampath Bandla <sampath@pixelvide.com>
2023-07-26 21:38:43 +0530

    Updated Code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+
+dotenv.config();
+app.listen(process.env.PORT || 3000, function (err) {
+    if (err) throw err;
+    console.log("App Started");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.all("/register", function (req, res) {
+    try {
+        res.json(req.body);
+    } catch (err) {
+        res.json({ error: "Something Went Wrong!" });
+    }
+});
